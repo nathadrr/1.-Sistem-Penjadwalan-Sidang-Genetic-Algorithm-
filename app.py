7f9102dc17b94a1e0b51c66d1373a60b84b3d260
nathadrr <nathadrr@gmail.com>
2026-06-27 12:32:17 +0700

    inisialisasi populasi awal

--- app.py
+++ app.py
@@ -1,34 +1,189 @@
 import streamlit as st
 import pandas as pd
+import random
 
-st.title('Sistem Penjadwalan Sidang Menggunakan Genetic Algorithm')
+def preprocessing_availability(df):
 
-#File upload menggunakan format .xlsx (excel)
-uploaded_file = st.file_uploader(
-    "Upload file Excel",
+    availability = {}
+
+    for dosen in df.columns[1:]:
+        dosen = dosen.strip()
+
+        slot_tersedia = set()
+
+        for _, row in df.iterrows():
+
+            if row[dosen] == 1:
+                slot_tersedia.add(int(row["id_slot"]))
+
+        availability[dosen] = slot_tersedia
+
+    return availability
+
+def preprocessing_mahasiswa(df):
+
+    mahasiswa = {}
+
+    for _, row in df.iterrows():
+
+        dosen = set()
+
+        for kolom in [
+            "dosbing1",
+            "dosbing2",
+            "dosenpenguji1",
+            "dosenpenguji2"
+        ]:
+
+            if pd.notna(row[kolom]):      # hanya jika tidak kosong
+                dosen.add(str(row[kolom]).strip())
+
+        mahasiswa[int(row["id_mhs"])] = {
+            "dosen": dosen
+        }
+
+    return mahasiswa
+
+def check_availability(id_mhs, slot, mahasiswa, availability):
+
+    dosen = mahasiswa[id_mhs]["dosen"]
+
+    for d in dosen:
+
+        if slot not in availability[d]:
+            return False
+
+    return True
+
+def check_conflict(chromosome, slot, id_mhs, mahasiswa):
+
+    dosen_baru = mahasiswa[id_mhs]["dosen"]
+
+    for mhs in chromosome[slot]:
+
+        if mhs == 0:
+            continue
+
+        dosen_lama = mahasiswa[mhs]["dosen"]
+
+        if dosen_baru & dosen_lama:
+            return False
+
+    return True
+
+def get_empty_room(chromosome, slot):
+
+    for r in range(4):
+
+        if chromosome[slot][r] == 0:
+            return r
+
+    return None
+
+def generate_initial_population(population_size, mahasiswa, availability):
+
+    population = []
+
+    jumlah_slot = 45
+    jumlah_ruangan = 4
+
+    daftar_mahasiswa = list(mahasiswa.keys())
+
+    for _ in range(population_size):
+
+        chromosome = [[0 for _ in range(jumlah_ruangan)]
+                      for _ in range(jumlah_slot)]
+
+        random.shuffle(daftar_mahasiswa)
+
+        for id_mhs in daftar_mahasiswa:
+
+            slot_list = list(range(1, jumlah_slot + 1))
+            random.shuffle(slot_list)
+
+            ditempatkan = False
+
+            for slot in slot_list:
+
+                if not check_availability(
+                    id_mhs,
+                    slot,
+                    mahasiswa,
+                    availability
+                ):
+                    continue
+
+                room = get_empty_room(
+                    chromosome,
+                    slot - 1
+                )
+
+                if room is None:
+                    continue
+
+                if not check_conflict(
+                    chromosome,
+                    slot - 1,
+                    id_mhs,
+                    mahasiswa
+                ):
+                    continue
+
+                chromosome[slot - 1][room] = id_mhs
+                ditempatkan = True
+                break
+
+            if not ditempatkan:
+                print(f"Mahasiswa {id_mhs} gagal ditempatkan.")
+
+        population.append(chromosome)
+
+    return population
+
+st.title("Sistem Penjadwalan Sidang Menggunakan Genetic Algorithm")
+
+uploaded_availability = st.file_uploader(
+    "Upload Ketersediaan Dosen",
     type=["xlsx"]
 )
 
-if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file)
-    st.dataframe(df)
+uploaded_mahasiswa = st.file_uploader(
+    "Upload Data Mahasiswa",
+    type=["xlsx"]
+)
 
-#preprocessing untuk file ketersediaan dosen
-availability = {}
+if uploaded_availability and uploaded_mahasiswa:
 
-for dosen in df.columns[1:]:
+    df_availability = pd.read_excel(uploaded_availability)
+    df_mahasiswa = pd.read_excel(uploaded_mahasiswa)
 
-    slot_tersedia = set()
+    availability = preprocessing_availability(df_availability)
+    mahasiswa = preprocessing_mahasiswa(df_mahasiswa)
 
-    for _, row in df.iterrows():
-        if row[dosen] == 1:
-            slot_tersedia.add(int(row["id_slot"]))
+population_size = st.number_input(
+    "Jumlah Populasi",
+    min_value=1,
+    value=10,
+    step=1
+)
 
-    availability[dosen] = slot_tersedia
+if st.button("Generate Populasi Awal"):
 
-#Pengecekan hasil availability dalam format .json
-#st.subheader("Hasil Availability")
-#st.write(availability)
+    population = generate_initial_population(
+        population_size,
+        mahasiswa,
+        availability
+    )
 
-#memastikan benar dalam format array
-#st.write(availability["D001"])
+    st.success("Populasi berhasil dibuat!")
+
+    for i, chromosome in enumerate(population, start=1):
+
+        st.subheader(f"Populasi {i}")
+
+        for slot, ruang in enumerate(chromosome, start=1):
+            st.text(f"Slot {slot:2d} : {ruang}")
+
+        st.divider()
+
+    
